5df466f6606bd1cb6350c596f35d6bdb905a3f01
mantanquzzmo <quzzmo@hotmail.com>
2020-01-07 17:01:42 +0100

    moves selector top for better UX

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,27 +3,6 @@ import React from "react";
 const Form = props => {
   return (
     <form onSubmit={props.onSubmitHandler}>
-      <label htmlFor="weight"></label>
-      <input
-        type="number"
-        required
-        placeholder={props.method === 'metric' ? "Weight in kgs" : "Weight in lbs"}
-        value={props.weight}
-        name="weight"
-        id="weight"
-        onChange={props.onChangeHandler}
-      />
-      <label htmlFor="height"></label>
-      <input
-        type="number"
-        required
-        placeholder={props.method === 'metric' ? "Height in cm" : "Height in inches"}
-        value={props.height}
-        name="height"
-        id="height"
-        onChange={props.onChangeHandler}
-      />
-      <br/>
       <select
         id="method"
         method={props.method}
@@ -33,8 +12,38 @@ const Form = props => {
         <option value="metric">Metric System</option>
         <option value="imperial">Imperial System</option>
       </select>
-      <br/>
-      <button id="calculate" class="button black">Calculate BMI</button>
+      <label htmlFor="weight"></label>
+      <input
+        type="number"
+        required
+        placeholder={
+          props.method === "metric" ? "Weight in kgs" : "Weight in lbs"
+        }
+        value={props.weight}
+        name="weight"
+        id="weight"
+        class="weight"
+        onChange={props.onChangeHandler}
+      />
+      <label htmlFor="height"></label>
+      <input
+        type="number"
+        required
+        placeholder={
+          props.method === "metric" ? "Height in cm" : "Height in inches"
+        }
+        value={props.height}
+        name="height"
+        id="height"
+        class="height"
+        onChange={props.onChangeHandler}
+      />
+      <br />
+
+      <br />
+      <button id="calculate" class="button black">
+        Calculate BMI
+      </button>
     </form>
   );
 };
